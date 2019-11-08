dd74a8c51cf5b3ae13a0535807d61e17dceca087
AlbertQM <king4spam@ymail.com>
2019-11-08 21:10:28 +0000

    Add Member model and migration

--- signup/models.py
+++ signup/models.py
@@ -1,1 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+# Extend User class to inherit useful fields.
+# Namely username, password and email
+class Member(User):
+    # Optional field
+    date_of_birth = models.DateField(blank=True)
+    def __str__(self):
+        return "Username: {}".format(self.username)
