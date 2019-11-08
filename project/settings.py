7cd8467b43ad35bf63d4006900ba65e240cf509a
AlbertQM <king4spam@ymail.com>
2019-11-08 20:00:07 +0000

    Fix path for templates in root dir

--- project/settings.py
+++ project/settings.py
@@ -56,10 +56,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'project.urls'
 
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
