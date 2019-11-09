6e765ef26b20286e4578a73e108f7e2fac88fb8c
AlbertQM <king4spam@ymail.com>
2019-11-09 09:06:25 +0000

    Implement signup (Users can now register via UI)

--- signup/urls.py
+++ signup/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signupPage, name='sign up page')
+    path('signup/', views.signupPage, name='sign up page'),
+    path('registration/', views.signupRequest, name='registration')
 ]
--- signup/views.py
+++ signup/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.template import loader
+from django.db import IntegrityError
+
+from signup.models import Member
+
+
 
 def signupPage(request):
     template = loader.get_template('signup/signup.html')
     return HttpResponse(template.render({}, request))
+
+def signupRequest(request):
+    if 'email' in request.POST:
+        email = request.POST['email']
+
+    if 'username' in request.POST and 'password' in request.POST:
+        username = request.POST['username']
+        pw = request.POST['password']
+
+        user = Member(username=username, email=email)
+        user.set_password(pw)
+        
+        try: user.save()
+        except IntegrityError: raise Http404('Username {} already taken: Usernames must be unique'.format(username))
+        return JsonResponse({'username': username})
+
+    else:
+        raise Http404('Missing required POST data.')
