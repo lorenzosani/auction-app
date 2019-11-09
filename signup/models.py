fbc4c36285d100bfabf63705c87733c68a5188bd
AlbertQM <king4spam@ymail.com>
2019-11-09 10:57:12 +0000

    Make date_of_birth a required field (updated initial migration)

--- signup/models.py
+++ signup/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Extend User class to inherit useful fields.
 # Namely username, password and email
 class Member(User):
-    # Optional field
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField()
     def __str__(self):
         return "Username: {}".format(self.username)
